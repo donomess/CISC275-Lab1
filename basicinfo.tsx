e9468e447dd92566a503312efb47577f21810294
Tyran Rice Jr <tyranjr@udel.edu>
2023-02-13 13:36:52 -0500

    Done

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,11 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+// Un-comment this and fill in the info
+const yourName : String = "Tyran Rice Jr.";
+let yourPronouns : String = "He/Him/His";
+const yourMajor : String = "Computer Science";
+let classification : String = "Soph";
+let oneExcite : String = "Learning how to make simple and advanced software and applications";
+let anyConcerns : String = "Speed of curriculum, group projects being one sided with collobration";
+let funFact : String = "I love rice and gaming";
