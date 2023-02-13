744f5528853fa2e05f76659ea21a66ed1caf3c4a
amansjot <41126421+amansjot@users.noreply.github.com>
2023-02-13 11:43:11 -0500

    Update basicinfo.tsx

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -1,14 +1,13 @@
-/* This is a basic file we are using to familiarize ourselves with you! 
-Simply un-comment these
+/* This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-
-const yourName : String = "Aman Singh";
-let yourPronouns : String = "he/him";
-const yourMajor : String = "Computer Science";
-let classification : String = "soph";
-let oneExcite : String = "I'm excited to learn React, which is in high demand.";
-let anyConcerns : String = "I'm worried that it might be too much content to learn properly.";
-let funFact : String = "I studied abroad in Hawaii for CS!";
-
+/*Un-comment this and fill in the info
+const yourName : String = "Your name";
+let yourPronouns : String = "Pro/Nouns";
+const yourMajor : String = "Your major";
+let classification : String = "freshman/soph/junior/senior/supersenior";
+let oneExcite : String = "One thing you're excited about in this course";
+let anyConcerns : String = "If anything, list what you may be worried about in this course";
+let funFact : String = "And the classic, list one fun fact about yourself";
+*/
