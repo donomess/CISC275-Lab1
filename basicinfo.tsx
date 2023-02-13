62b28fb8368ca0468ff2b48056e3fd94d97fc04a
Jadyn Worthington <jadynwor@udel.edu>
2023-02-13 11:02:55 -0500

    again

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -8,5 +8,5 @@ const yourMajor : String = "Comp Sci";
 let classification : String = "Junior";
 let oneExcite : String = "Learning how to build websites and better handling github";
 let anyConcerns : String = "git and github with merge conflicts";
-let funFact : String = "I like to go to car meets";
+let funFact : String = "I like to go to car meets and go out";
 
