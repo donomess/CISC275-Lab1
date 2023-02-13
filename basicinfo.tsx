7b34cf5eec2948989d09444b1134a2541b02af50
Mikaylla <mikahas@udel.edu>
2023-02-13 13:05:48 -0500

    Added names and info

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -1,13 +1,13 @@
-/* Mikaylla This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
+/* This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
 /*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
+const yourName : String = "Mikaylla Haskins";
+let yourPronouns : String = "She/Her";
+const yourMajor : String = "Computer Science";
+let classification : String = "Senior";
+let oneExcite : String = "Coding";
+let anyConcerns : String = "Projects and workload";
+let funFact : String = "Favorite color is yellow";
 */
