3fa9f7b53e465dd11f3e97703b88fd39f4313f42
MichaelBrandon-UDel <70721605+MichaelBrandon-UDel@users.noreply.github.com>
2023-02-13 11:30:45 -0500

    Update basicinfo.tsx

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -3,11 +3,11 @@ below variables and update them with stuff about you. Yeah... I know... the clas
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
 /*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
+const yourName : String = "Michael Arocho";
+let yourPronouns : String = "He/him";
+const yourMajor : String = "Computer Science";
+let classification : String = "senior";
+let oneExcite : String = "Creating a website";
+let anyConcerns : String = "My competence level";
+let funFact : String = "I make 3D models in Blender";
 */
