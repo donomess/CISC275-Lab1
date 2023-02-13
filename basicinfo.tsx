525c912db412391ea4622a3119ebf4762fb46b35
Jadyn Worthington <jadynwor@udel.edu>
2023-02-13 10:48:15 -0500

    completed file

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,11 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+const yourName : String = "Jadyn Worthington";
+let yourPronouns : String = "Him/He";
+const yourMajor : String = "Comp Sci";
+let classification : String = "Junior";
+let oneExcite : String = "Learning how to build websites and better handling github";
+let anyConcerns : String = "git and github with merge conflicts";
+let funFact : String = "I like to go to car meets";
+
