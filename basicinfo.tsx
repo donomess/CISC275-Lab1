84888eef99bedf796dde5c7a08a6a46a14cbfdbc
Farhan Ohe <farhanto@udel.edu>
2023-02-17 11:00:02 -0500

    Updated existing textfile removed other one i created.

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,12 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
+Un-comment this and fill in the info
+const yourName : String = "Farhan Ohe"
+let yourPronouns : String = "he him";
+const yourMajor : String = "Computer Science";
+let classification : String = "soph";
+let oneExcite : String = "I am excited to learn more about web and app dev and just software engineering in general cause a lot of these classes have been so broad, finally focusing on something is nice.";
+let anyConcerns : String = "falling behind";
+let funFact : String = "I work as a IT anaylist, but I am trying to get into a software engineer positon. I also love cars and nashville hot chicken.";
 */
