bf501a83c8179ed9dc16e520fe8cffd57d9204e2
KEVIN CHAU <kevinch@udel.edu>
2023-02-17 10:59:27 -0500

    changed my info ls

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -1,13 +1,13 @@
 /* This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
-everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
-
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
+everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit.
 */
+
+const yourName : String = "Kevin Chau";
+let yourPronouns : String = "he/him";
+const yourMajor : String = "Computer Science";
+let classification : String = "sophomore";
+let oneExcite : String = "One thing you're excited about in this course: Learning how to work with a website!";
+let anyConcerns : String = "If anything, list what you may be worried about in this course: I am worried about merge conflicts!";
+let funFact : String = "And the classic, list one fun fact about yourself: I play Valorant for UD";
+
