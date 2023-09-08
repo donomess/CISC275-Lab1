707634b318a4f9584b9e374768dac200ea7b41ba
Brandon Marafino <Marafino@udel.edu>
2023-09-08 13:20:44 -0400

    On branch newFeature  Changes to be committed: 	modified:   basicinfo.tsx

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,12 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+Un-comment this and fill in the info
+const yourName : String = "Brandon Marafino";
+let yourPronouns : String = "He/Him";
+const yourMajor : String = "Computer Engineering";
+let classification : String = "senior";
+let oneExcite : String = "type script";
+let anyConcerns : String = "Nope" ;
+let funFact : String = "I self taugh my self js for my internship this summer";
+
