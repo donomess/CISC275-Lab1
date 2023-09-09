089298cbb61a50fa08fd03d2214fb1ff0ccfbe40
ZONGLIN <ZONGLIN@UDEL.EDU>
2023-09-09 00:27:35 -0400

    add my info

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -3,11 +3,11 @@ below variables and update them with stuff about you. Yeah... I know... the clas
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
 /*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
+const yourName : String = "Zonglin Wu";
+let yourPronouns : String = "Handsome";
+const yourMajor : String = "CS";
+let classification : String = "junior";
+let oneExcite : String = "make software app by myself";
+let anyConcerns : String = "Nothing very concerned";
+let funFact : String = "I'm a DFO game pro";
 */
